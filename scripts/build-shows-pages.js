93aebfebbddbe2c72ed2e6de9c6544901c112b71
Xiaoyi She <xiaoyi.s.she@gmail.com>
2024-02-12 09:07:11 -0800

    added style to show page

--- scripts/build-shows-pages.js
+++ scripts/build-shows-pages.js
@@ -24,20 +24,19 @@
 
     const container = document.getElementById('arrayDisplay');
 
-    // Create an unordered list (ul) element
+   
     const ul = document.createElement('ul');
 
-    // Iterate through the array and create list items (li) for each entry
+   
     infoArray.forEach(item => {
       const li = document.createElement('li');
       li.textContent = `Name: ${item.date}, Age: ${item.venue}, City: ${item.location}`;
       ul.appendChild(li);
     });
 
-    // Append the unordered list to the container
+    
     container.appendChild(ul);
   }
 
-  // Call the displayArray function to show the array on the page
   displayArray();
 
