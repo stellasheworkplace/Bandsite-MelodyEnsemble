413eafc08c40c58deab00519f49bae2d3925a4d4
Xiaoyi She <xiaoyi.s.she@gmail.com>
2024-02-16 10:21:28 -0800

    added style to show page

--- scripts/build-shows-pages.js
+++ scripts/build-shows-pages.js
@@ -1,42 +1,116 @@
 
-  // Sample array of information
-  const infoArray = [
-    {
-        date: "Mon Sept 09 2024",
-        venue: "Ronald Lane",
-      location:"San Francisco, CA"
-    
-    },
-    {
-        date: "Tue Sept 17 2024",
-        venue: "Pier 3 East",
-      location:"San Francisco, CA"
-       
-    },
-    {
-       date: "Sat Oct 12 2024",
-        venue: "View Lounge",
-      location:"San Francisco, CA"
-    }
-  ];
+// Sample array of information
+const shows = [
+  {
+    date: "Mon Sept 09 2024",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA"
 
-  function displayArray() {
+  },
+  {
+    date: "Tue Sept 17 2024",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA"
 
-    const container = document.getElementById('arrayDisplay');
-
-   
-    const ul = document.createElement('ul');
-
-   
-    infoArray.forEach(item => {
-      const li = document.createElement('li');
-      li.textContent = `Name: ${item.date}, Age: ${item.venue}, City: ${item.location}`;
-      ul.appendChild(li);
-    });
-
-    
-    container.appendChild(ul);
+  },
+  {
+    date: "Sat Oct 12 2024",
+    venue: "View Lounge",
+    location: "San Francisco, CA"
   }
+];
 
-  displayArray();
+// const showEL = document.querySelectorAll(".show");
 
+//showAll(shows);
+
+//function showAll(shows) {
+  // showEL.innerHTML = "";
+
+  // for (let i = 0; i < shows.length; i++) {
+  //   const show = shows[i];
+
+  //   console.log(displayShow(show));
+  // }
+//}
+
+for(let i = 0; i < shows.length; i++){
+  const show = shows[i];
+
+  displayShow(show);
+}
+
+/*<article "show">
+
+  <div class = "show__date">
+  <h3>DATE</h3>
+  <h2></h2>
+  </div>
+
+  <div class = "show__venue">
+  <h3>DATE</h3>
+  <h2></h2>
+  </div>
+
+  <div class = "lshow__ocation">
+  <h3>DATE</h3>
+  <h2></h2>
+  </div> 
+
+</article>*/
+
+function displayShow(shows) {
+  const showsEL = document.createElement("section");
+  showsEL.classList.add("show");
+
+  // <show__date>
+  const showsDate = document.createElement("div");
+  showsDate.classList.add("show__date");
+
+  const showsTitle = document.createElement("h3");
+  showsTitle.classList.add("show__date-title");
+  showsTitle.innerText = "DATE";
+
+  const showsSubTitle = document.createElement("h2");
+  showsSubTitle.classList.add("show__date-subtitle");
+  showsSubTitle.innerText = shows.date;
+
+  //show__venue
+  const showsVenue = document.createElement("div");
+  showsVenue.classList.add("show__venue");
+
+  const showsVenueTitle = document.createElement("h3");
+  showsVenueTitle.classList.add("show__venue-title");
+  showsVenueTitle.innerText = "VENUE";
+
+  const showsVenueSubTitle = document.createElement("h2");
+  showsVenueSubTitle.classList.add("show__venue-subtitle");
+  showsVenueSubTitle.innerText = shows.venue;
+
+  //show__location
+  const showsLocation = document.createElement("div");
+  showsLocation.classList.add("show__ocation");
+
+  const showsLocationTitle = document.createElement("h3");
+  showsLocationTitle.classList.add("show__ocation-title");
+  showsLocationTitle.innerText = "LOCATION";
+
+  const showsLocationSubTitle = document.createElement("h2");
+  showsLocationSubTitle.classList.add("show__date-subtitle");
+  showsLocationSubTitle.innerText = shows.location;
+
+  showsEL.appendChild(showsDate);
+  showsEL.appendChild(showsVenue);
+  showsEL.appendChild(showsLocation);
+
+  showsDate.appendChild(showsTitle);
+  showsDate.appendChild(showsSubTitle);
+
+  showsVenue.appendChild(showsVenueTitle);
+  showsVenue.appendChild(showsVenueSubTitle);
+
+  showsLocation.appendChild(showsLocationTitle);
+  showsLocation.appendChild(showsLocationSubTitle);
+
+  document.querySelector("#showTime").appendChild(showsEL);
+}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,67 +1,126 @@
 const users = [
     {
-        name:"Victor Pinto",
-        comment:"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+        image: "/",
+        name: "Victor Pinto",
+        timestampe: "11/02/2023",
+        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
     },
     {
-        name:"Christina Cebrera",
-        comment:"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+        image: "/",
+        name: "Christina Cebrera",
+        timestampe: "10/28/2023",
+        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
     },
     {
-        name:"Isaac Tadesse",
-        comment:"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+        image: "/",
+        name: "Isaac Tadesse",
+        timestampe: "10/20/2023",
+        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     }
 ];
 
-const userEL = document.querySelector(".users");
+const usersEL = document.querySelector(".users");
 
 showALLComments(users);
 
-function showALLComments(users){
-    userEL.innerHTML = "";
+function showALLComments(users) {
+    usersEL.innerHTML = "";
 
-    for(let i = 0; i < users.length; i++){
+    for (let i = 0; i < users.length; i++) {
         const user = users[i];
 
         displayUser(user);
     }
 }
+/* <section class="user">
+   
+    <img class="user__avatar" src="/" alt="avatar"></img>
 
-function displayUser(user){
+    <div class="user__container">
+        <div class="user__container-namendate">
+            <h3 class="user__name"></h3>
+            <h3 class="user__date"></h3>
+        </div>
 
-    const userEL = document.createElement("article");
+        <h3 class="user__comment"></h3>
+
+    </div>
+</section> */
+
+function displayUser(user) {
+
+    const userEL = document.createElement("section");
     userEL.classList.add("user");
 
+    const userAvatar = document.createElement("img");
+    userAvatar.classList.add("user__avatar");
+    userAvatar.src = "./assets/Images/icon-like.jpeg";
+    userAvatar.alt = "avatar";
+
     const userInfo = document.createElement("div");
-    userInfo.classList.add("user__info-container");
+    userInfo.classList.add("user__container");
+
+    const userNameDate = document.createElement("div");
+    userNameDate.classList.add("user__container-namedate");
 
     const userName = document.createElement("h3");
-    userName.classList.add(user_name);
+    userName.classList.add("user__name");
     userName.innerText = user.name;
 
-    const userComment = document.createElement("div");
-    userName.classList.add(user_comment);
-    userName.innerText = user.comment;
+    const userDate = document.createElement("h3");
+    userDate.classList.add("user__date");
+    userDate.innerText = user.timestampe;
 
-    userInfo.appendChild(userName);
+    const userComment = document.createElement("h3");
+    userComment.classList.add("user__comment");
+    userComment.innerText = user.comment;
+
+    userEL.appendChild(userAvatar);
+    userEL.appendChild(userInfo);
+
+    userInfo.appendChild(userNameDate);
     userInfo.appendChild(userComment);
-    userEL.appendChild(userEL);
+
+    userNameDate.appendChild(userName);
+    userNameDate.appendChild(userDate);
+
+    usersEL.appendChild(userEL);
 }
 
+//related to form
 const addUser = document.getElementById("add-comment");
+const formErrors = document.getElementById("add-errors");
 
-addUser.addEventListener("submit",(event) => {
+addUser.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    formErrors.innerText = "";
+
+    //const currentTime = new Date();                
+    // const curdate = currentTime.getDate();
+    //document.getElementbyId("date-of-registration1").value = curdate;
+    // const image = event.target.image.value;
+    //const timestampe = event.target.timestampe.value;
+
     const name = event.target.name.value;
+
     const comment = event.target.comment.value;
 
+    if (name === "") {
+        formErrors.innerText = "Name can not be empty";
+
+        return;
+    }
+
     const newUser = {
+        // image: image,
         name: name,
+        // timestampe: curdate,
         comment: comment
     }
 
     users.push(newUser);
+
     showALLComments(users);
 });
 
