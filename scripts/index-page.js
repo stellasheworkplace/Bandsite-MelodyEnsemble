07f34e3714be89099d985cab3d691a645f990849
Xiaoyi She <xiaoyi.s.she@gmail.com>
2024-02-18 21:13:42 -0800

    fix style

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -22,6 +22,7 @@ const users = [
     }
 ];
 
+
 const usersEL = document.querySelector(".users");
 
 showALLComments(users);
@@ -99,12 +100,6 @@ addUser.addEventListener("submit", (event) => {
 
     formErrors.innerText = "";
 
-    //const currentTime = new Date();                
-    // const curdate = currentTime.getDate();
-    //document.getElementbyId("date-of-registration1").value = curdate;
-    // const image = event.target.image.value;
-    //const timestampe = event.target.timestampe.value;
-
     const name = event.target.name.value;
 
     const comment = event.target.comment.value;
@@ -116,9 +111,9 @@ addUser.addEventListener("submit", (event) => {
     }
 
     const newUser = {
-        // image: image,
+        
         name: name,
-        // timestampe: curdate,
+        timestampe: new Date().toLocaleDateString(),
         comment: comment
     }
 
@@ -126,5 +121,76 @@ addUser.addEventListener("submit", (event) => {
 
     showALLComments(users);
 });
+
+//The code below is my idea of the sprint-3, but not works property at this point
+//I am thinking about the displayUser() method should be fixed to have a list to contain data from the api...
+
+
+// const usersEL = document.querySelector(".users");
+
+// async function displayUser() {
+
+//     const comments = await bandSiteApi.getComments();
+//     console.log(comments);
+
+//     commentListEl.innerHTML = "";
+
+//     comments.forEach((user) => {
+
+//     const userEL = document.createElement("section");
+//     userEL.classList.add("user");
+
+//     const userAvatar = document.createElement("img");
+//     userAvatar.classList.add("user__avatar");
+//     userAvatar.src = "./assets/Images/icon-like.jpeg";
+//     userAvatar.alt = "avatar";
+
+//     const userInfo = document.createElement("div");
+//     userInfo.classList.add("user__container");
+
+//     const userNameDate = document.createElement("div");
+//     userNameDate.classList.add("user__container-namedate");
+
+//     const userName = document.createElement("h3");
+//     userName.classList.add("user__name");
+//     userName.innerText = user.name;
+
+//     const userDate = document.createElement("h3");
+//     userDate.classList.add("user__date");
+//     userDate.innerText = user.timestampe;
+
+//     const userComment = document.createElement("h3");
+//     userComment.classList.add("user__comment");
+//     userComment.innerText = user.comment;
+
+//     userEL.appendChild(userAvatar);
+//     userEL.appendChild(userInfo);
+
+//     userInfo.appendChild(userNameDate);
+//     userInfo.appendChild(userComment);
+
+//     userNameDate.appendChild(userName);
+//     userNameDate.appendChild(userDate);
+
+//     usersEL.appendChild(userEL);
+//     });
+// }
+
+// const formEL = document.querySelector(".comment");
+
+// formEL.addEventListener("submit", async function(event) {
+//     event.preventDefault();
+//     console.log(event.target.comment.value);
+//     const response = await bandSiteApi.postComments({
+//       name: event.target.name.value ,
+//       comment: event.target.comment.value
+//       });
+  
+//     getComments();
+  
+//     formEL.reset();
+//   });
+  
+//   getComments();
 
 
